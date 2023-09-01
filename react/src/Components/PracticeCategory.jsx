74b919d9960e4e5b413d0daa40fce2b2f14cd906
Leo Lee <1155158461@link.cuhk.edu.hk>
2023-09-01 13:13:54 +0800

    style: formatting components

--- react/src/Components/PracticeCategory.jsx
+++ react/src/Components/PracticeCategory.jsx
@@ -1,9 +1,19 @@
-export default function PracticeCategory({ category }) {
+export default function PracticeCategory({
+  category,
+  currentCategory,
+  setCurrentCategory,
+}) {
   return (
     <div className="flex flex-row items-center bg-gray-200 gap-4 py-1 px-2 rounded-lg">
       {category.map((item) => {
         return (
-          <div className="text-2xl text-gray-700" key={item}>
+          <div
+            onClick={() => setCurrentCategory(item)}
+            className={`text-2xl cursor-pointer ${
+              currentCategory === item ? "text-blue-500" : "text-gray-700"
+            }`}
+            key={item}
+          >
             {item}
           </div>
         );
--- react/src/Layouts/DefaultLayout.jsx
+++ react/src/Layouts/DefaultLayout.jsx
@@ -1,19 +1,17 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../Contexts/ContextProvider.jsx";
 import axiosClient from "../axios-client.js";
-import { useEffect } from "react";
-import Record from "../Components/Record.jsx";
-import PracticeCategory from "../Components/PracticeCategory.jsx";
+import { useEffect, useState } from "react";
 
 //"中文速打", "倉頡練習", "粵拼練習"
 export default function DefaultLayout() {
   const { user, token, setUser, setToken, notification } = useStateContext();
-  const header = {
+  const pages = {
     speedTyping: "中文速打",
     cangjie: "倉頡練習",
     jyutping: "粵拼練習",
   };
-  const category = ["字根訓練", "字形訓練", "單字訓練"];
+  const [currentPage, setCurrentPage] = useState("speedTyping");
 
   useEffect(() => {
     axiosClient.get("/user").then(({ data }) => {
@@ -43,26 +41,23 @@ export default function DefaultLayout() {
       <div className="w-full p-6">
         <header className="flex flex-col justify-center items-center mt-2">
           <div className="flex flex-row gap-10 text-4xl font-bold">
-            {Object.entries(header).map(([key, value]) => {
+            {Object.entries(pages).map(([key, value]) => {
               return (
                 <Link
+                  onClick={() => setCurrentPage(key)}
                   key={key}
                   to={"/" + key}
-                  className="hover:text-blue-500 hover:-translate-y-0.5 transition-all duration-50"
+                  className={`hover:text-blue-300 hover:-translate-y-0.5 transition-all duration-500 ${
+                    currentPage === key ? "text-blue-500" : "text-black"
+                  }`}
                 >
                   {value}
                 </Link>
               );
             })}
           </div>
-          <div className="mt-4 flex flex-row items-center gap-4">
-            <Record speed={70} accuracy={0.95} />
-            <PracticeCategory category={category} />
-          </div>
         </header>
-        <main>
-          <Outlet />
-        </main>
+        <Outlet />
       </div>
     </div>
   );
