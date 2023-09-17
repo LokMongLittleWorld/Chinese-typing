af43668c9cf6a68e0c2d8963c12d1a37db2581fb
Leo Lee <1155158461@link.cuhk.edu.hk>
2023-09-18 00:12:56 +0900

    fix: jyutping typo

--- react/src/Pages/Jyutping/Index.jsx
+++ react/src/Pages/Jyutping/Index.jsx
@@ -8,7 +8,7 @@ import Character from "../../Components/Character.jsx";
 import AmountSelector from "../../Components/AmountSelector.jsx";
 
 //prettier-ignore
-const intials = ["b", "p", "m", "f", "d", "t", "n", "l", "g", "k", "ng", "h", "gw", "kw", "w", "z", "c", "s", "j"];
+const initials = ["b", "p", "m", "f", "d", "t", "n", "l", "g", "k", "ng", "h", "gw", "kw", "w", "z", "c", "s", "j"];
 export default function Index() {
   const category = ["聲母訓練", "韻母訓練", "單字訓練"];
   const [currentCategoryIndex, setCurrentCategoryIndex] = useState(
@@ -52,7 +52,7 @@ export default function Index() {
       <main className="mt-2">
         {/* TODO: hover to display speed and accuracy, color to indicate proficiency */}
         <section className="flex flex-row gap-4 justify-center text-2xl">
-          {intials.map((radical) => {
+          {initials.map((radical) => {
             return <div key={radical}>{radical.toUpperCase()}</div>;
           })}
         </section>
