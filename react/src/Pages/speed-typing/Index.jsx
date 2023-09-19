029b7c215adff3b6dfc01f5af7fa2db097c7d299
Leo Lee <1155158461@link.cuhk.edu.hk>
2023-09-20 00:20:27 +0900

    feat: basic article display

--- react/src/Pages/speed-typing/Index.jsx
+++ react/src/Pages/speed-typing/Index.jsx
@@ -1,7 +1,16 @@
+import Essays from "../../../static/speed-typing/twelve_model_essays.json";
+import ArticleDisplayCard from "../../Components/ArticleDisplayCard.jsx";
+
 export default function Index() {
   return (
     <main className="flex justify-center items-center mt-8">
-      <div className="grid grid-cols-3 gap-4 w-[80%]"></div>
+      <div className="grid grid-cols-3 gap-4 w-[80%]">
+        {Object.values(Essays).map((essay, index) => (
+          <div key={index}>
+            <ArticleDisplayCard title={essay?.title} content={essay.content} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
