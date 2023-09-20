a0547c977e3428e0b0ce04e816411931d30dd98f
Leo Lee <1155158461@link.cuhk.edu.hk>
2023-09-20 12:48:12 +0900

    feat: 3 line display

--- react/src/Components/ArticleDisplayCard.jsx
+++ react/src/Components/ArticleDisplayCard.jsx
@@ -4,7 +4,9 @@ export default function ArticleDisplayCard({ title, content }) {
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {title}
       </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{content}</p>
+      <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-3">
+        {content}
+      </p>
     </div>
   );
 }
