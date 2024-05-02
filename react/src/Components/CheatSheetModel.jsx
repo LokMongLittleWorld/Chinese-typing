77d12fb13392ca114de90fad71a4089006fded4f
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-05-02 21:12:59 +0800

    feat: cheatsheet

--- react/src/Components/CheatSheetModel.jsx
+++ react/src/Components/CheatSheetModel.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import CheatSheet from "../../static/cangjie/cheatsheet.json";
+import Radical from "../../static/cangjie/radicals.json";
 import Model from "./Model.jsx";
 
 export default function cheatSheetModel({ showModal = false, setShowModal }) {
@@ -8,7 +10,50 @@ export default function cheatSheetModel({ showModal = false, setShowModal }) {
   return (
     <Model showModal={showModal}>
       <Model.Header handleOnClick={handleOnClick} title="倉頡輔助字形表" />
-      <Model.Content />
+      <Model.Content>
+        <div className="flex flex-row text-3xl text-gray-700 gap-4 p-4">
+          {Object.entries(CheatSheet)
+            .slice(0, -1)
+            .map(([key, value], index) => {
+              return <CheatSheetGrid title={key} value={value} key={index} />;
+            })}
+        </div>
+        {/*  last the object in CheatSheet*/}
+        <div className="text-3xl text-gray-700 p-4 pt-0">
+          <CheatSheetGrid
+            title={Object.entries(CheatSheet).pop()[0]}
+            value={Object.entries(CheatSheet).pop()[1]}
+          />
+        </div>
+      </Model.Content>
     </Model>
   );
 }
+
+const CheatSheetGrid = ({ title, value }) => {
+  return (
+    <div className="flex flex-col items-center bg-gray-100 rounded-md p-2">
+      <div className="m-2">{title}</div>
+      <div>
+        {Object.entries(value).map(([char, glyph], index) => {
+          return (
+            <div className="flex flex-row gap-2 p-0.5" key={index}>
+              <div>{Radical[char].toUpperCase()}</div>
+              <div>{char}</div>
+              <div className="flex flex-row ml-2">
+                {/*display the element in glyph one by one*/}
+                {glyph.map((element, index) => {
+                  return (
+                    <div key={index} className="flex font-glyph gap-1">
+                      {element}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- react/src/Components/Model.jsx
+++ react/src/Components/Model.jsx
@@ -4,12 +4,10 @@ const Model = ({ showModal = false, children }) => {
   return (
     <>
       {showModal && (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-y-auto overflow-x-hidden z-100 justify-center items-center">
-          <div className="relative p-4 w-full max-w-2xl max-h-full">
-            {/* main modal content */}
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-              {children}
-            </div>
+        <div className="fixed top-0 right-0 left-0 bottom-0 z-100 flex justify-center items-center select-none">
+          {/* main modal content */}
+          <div className="bg-gray-50 rounded-lg shadow dark:bg-gray-700 items-center justify-center">
+            {children}
           </div>
         </div>
       )}
@@ -19,8 +17,8 @@ const Model = ({ showModal = false, children }) => {
 
 const Header = ({ title, handleOnClick }) => {
   return (
-    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-      <h3 className="text-xl font-semibold text-gray-700 dark:text-white">
+    <div className="flex items-center justify-between en p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+      <h3 className="text-4xl font-semibold text-gray-700 dark:text-white">
         {title}
       </h3>
       <CloseButton handleOnClick={handleOnClick} />
@@ -29,23 +27,7 @@ const Header = ({ title, handleOnClick }) => {
 };
 
 const Content = ({ children }) => {
-  return (
-    <div className="p-4 md:p-5 space-y-4">
-      {children}
-      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-        With less than a month to go before the European Union enacts new
-        consumer privacy laws for its citizens, companies around the world are
-        updating their terms of service agreements to comply.
-      </p>
-      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-        The European Union’s General Data Protection Regulation (G.D.P.R.) goes
-        into effect on May 25 and is meant to ensure a common set of data rights
-        in the European Union. It requires organizations to notify users as soon
-        as possible of high-risk data breaches that could personally affect
-        them.
-      </p>
-    </div>
-  );
+  return <>{children}</>;
 };
 
 Model.Header = Header;
