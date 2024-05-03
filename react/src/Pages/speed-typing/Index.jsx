bdde37ad2e155bd9cdbadd2fbd6ef8b61d414719
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-05-03 21:17:32 +0800

    chore: renew example article

--- react/src/Pages/speed-typing/Index.jsx
+++ react/src/Pages/speed-typing/Index.jsx
@@ -1,4 +1,4 @@
-import Essays from "../../../static/speed-typing/twelve_model_essays.json";
+import Articles from "../../../static/speed-typing/copypasta.json";
 import ArticleDisplayCard from "../../Components/ArticleDisplayCard.jsx";
 import { Link } from "react-router-dom";
 
@@ -6,7 +6,7 @@ export default function Index() {
   return (
     <main className="flex justify-center items-center mt-8">
       <div className="grid grid-cols-3 gap-4 w-[80%]">
-        {Object.values(Essays).map((essay, index) => (
+        {Object.values(Articles).map((essay, index) => (
           <Link key={index} to={"/speed-typing/" + index}>
             <ArticleDisplayCard title={essay?.title} content={essay?.content} />
           </Link>
