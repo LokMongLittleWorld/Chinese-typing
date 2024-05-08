86b56f62a7b6e0b9d2001125a61ab264cdfbfb0c
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-05-08 20:57:24 +0800

    style: display empty if no article

--- react/src/Pages/speed-typing/Index.jsx
+++ react/src/Pages/speed-typing/Index.jsx
@@ -2,17 +2,29 @@ import ArticleDisplayCard from "../../Components/ArticleDisplayCard.jsx";
 import { Link } from "react-router-dom";
 import axiosClient from "../../axios-client.js";
 import { useEffect, useState } from "react";
+import Empty from "../../Components/Empty.jsx";
 
 export default function Index() {
-  const [articles, setArticles] = useState({});
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   // fetch articles
   useEffect(() => {
-    axiosClient.get("/article").then(({ data }) => {
-      setArticles(data.articles);
-    });
+    axiosClient
+      .get("/article")
+      .then(({ data }) => {
+        setArticles(data.articles);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
+  if (articles.length === 0 && !isLoading) {
+    return <Empty message="There is no article" />;
+  }
+
+  //TODO: lazy load
   return (
     <main className="flex justify-center items-center mt-8">
       <div className="grid grid-cols-3 gap-4 w-[80%]">
