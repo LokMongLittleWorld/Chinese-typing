08ea3e2930897ad22ac2f4a58cfc5e9be0349455
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-05-08 20:02:42 +0800

    refactor: move NotFound from page to component
    add props

--- react/src/Components/NotFound.jsx
+++ react/src/Components/NotFound.jsx
@@ -1,6 +1,6 @@
 import error404 from "../../../public/images/error404.jpeg";
 
-export default function NotFound() {
+export default function NotFound({ status = 404, message = "Not Found" }) {
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center flex-col gap-4 z-[-1]">
       <img
@@ -8,7 +8,9 @@ export default function NotFound() {
         alt="error 404"
         className="rounded-lg shadow-lg aspect-auto min-w-[40%] w-[400px]"
       />
-      <div className="font-nunito text-5xl text-gray-700">404 - Not Found</div>
+      <div className="font-nunito text-5xl text-gray-700">
+        {status} - {message}
+      </div>
     </div>
   );
 }
--- react/src/router.jsx
+++ react/src/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./Pages/Login.jsx";
 import Register from "./Pages/Register.jsx";
 import Users from "./Pages/Users.jsx";
-import NotFound from "./Pages/NotFound.jsx";
+import NotFound from "./Components/NotFound.jsx";
 import DefaultLayout from "./Layouts/DefaultLayout.jsx";
 import GuestLayout from "./Layouts/GuestLayout.jsx";
 import Index from "./Pages/speed-typing/Index.jsx";
