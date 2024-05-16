a4e7b176b3ae57511825f8b09d3e7dedd6ef4308
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-05-17 03:07:25 +0700

    style: add title and content

--- react/src/Pages/speed-typing/CreateArticle.jsx
+++ react/src/Pages/speed-typing/CreateArticle.jsx
@@ -4,36 +4,116 @@ import axiosClient from "../../axios-client.js";
 export default function CreateArticle() {
   const titleRef = createRef();
   const contentRef = createRef();
+  const [focusedArea, setFocusedArea] = useState(""); // ["title", "content"]
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [errors, setErrors] = useState({});
-
   const handleSubmit = () => {
-    console.log(errors);
-
-    // const values = {
-    //   title: titleRef.current.value,
-    //   content: contentRef.current.value,
-    // };
-
     const values = {
-      title: "test",
-      content: "你知吾知你好撚on9咩？",
+      title: title,
+      content: handleContent(content),
     };
 
     axiosClient.post("/article", values).then(({ data }) => {});
   };
+
+  const handleContent = (content) => {
+    //   for every last 12 characters doesn't contain a newline character, add a newline character
+    const contentArray = content.split("");
+    const newContentArray = [];
+    let lastNewLineCharacterIndex = 0;
+    for (let i = 0; i < contentArray.length; i++) {
+      newContentArray.push(contentArray[i]);
+      if (contentArray[i] === "\n") {
+        lastNewLineCharacterIndex = i + 1;
+      }
+      if (i - lastNewLineCharacterIndex + 1 === 4) {
+        newContentArray.push("\n");
+        lastNewLineCharacterIndex = i + 1;
+      }
+    }
+    return newContentArray.join("");
+  };
+
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center gap-4 rounded-lg mt-4">
-        <div>article</div>
-        <input ref={titleRef} type="title" placeholder="title" />
-        <input ref={contentRef} type="content" placeholder="content" />
+    <div className="relative flex justify-center items-center mt-4">
+      <div className="flex flex-col items-center justify-center gap-4 rounded-lg mt-4 bg-white shadow-sm w-[768px]">
+        {/*heading*/}
+        <div className="w-full en p-4 md:p-5 border-b rounded-t dark:border-gray-600 flex items-center justify-center">
+          <h3 className="text-4xl text-center font-semibold text-gray-700 dark:text-white select-none">
+            建立新的速打文章
+          </h3>
+        </div>
+        {/*title*/}
+        <div
+          className="text-4xl flex flex-row text-gray-700"
+          onClick={() => {
+            setFocusedArea("title");
+            titleRef.current.focus();
+          }}
+        >
+          <span>《</span>
+          <span>{title}</span>
+          {focusedArea === "title" ? (
+            <div className="transform translate-y-[4px] w-[3px] h-8 rounded-lg animate-typing bg-gray-500" />
+          ) : (
+            <div className="w-[3px]" />
+          )}
+          <span className="text-gray-400">{title ? "" : "標題"}</span>
+          <span>》</span>
+        </div>
+        {/*content*/}
+        <div
+          className="relative text-4xl w-[500px] h-[300px] rounded-lg border p-4 overflow-y-scroll"
+          onClick={() => {
+            setFocusedArea("content");
+            contentRef.current.focus();
+          }}
+        >
+          {content.split("")?.map((character, index) => {
+            return (
+              <>
+                {character === "\n" ? (
+                  <br key={index} />
+                ) : (
+                  <span key={index} className="text-gray-700">
+                    {character}
+                  </span>
+                )}
+                {focusedArea === "content" && index === content.length - 1 ? (
+                  // text cursor
+                  <span className="absolute transform translate-y-[4px] w-[3px] h-8 rounded-lg animate-typing bg-gray-500 inline-block" />
+                ) : null}
+              </>
+            );
+          })}
+          {focusedArea === "content" && content.length === 0 ? (
+            // text cursor
+            <span className="absolute transform translate-y-[4px] w-[3px] h-8 rounded-lg animate-typing bg-gray-500 inline-block" />
+          ) : (
+            <span className="inline-block" />
+          )}
+          <span className="text-gray-400">{content ? "" : "內容"}</span>
+        </div>
         <button
           onClick={handleSubmit}
           type="button"
-          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          className="text-sm text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"
         >
-          Submit
+          提交
         </button>
+        <input
+          ref={titleRef}
+          type="title"
+          className="opacity-0 absolute top-0 left-0"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          ref={contentRef}
+          placeholder="內容"
+          className="opacity-0 absolute top-0 left-0"
+          onChange={(e) => setContent(e.target.value)}
+        />
       </div>
     </div>
   );
