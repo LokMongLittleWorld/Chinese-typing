5abcd0f81627bb91cf59a691f45fb6b40137a105
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-05-25 16:22:09 +0800

    feat: include SelectComponent

---
+++ react/src/Components/SelectComponent.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Select from "react-select";
+
+const optionsSample = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+const customStyles = {
+  input: (base) => ({
+    ...base,
+    "input[type='text']:focus": { boxShadow: "none" },
+  }),
+  control: (provided) => ({
+    // class attribute : class=" css-i32vvf-control"
+    ...provided,
+    "borderWidth": "1px",
+    "borderColor": "inherit",
+    "boxShadow": "none",
+    "&:hover": {
+      borderWidth: "1px",
+      borderColor: "inherit",
+    },
+    "&:focus": {
+      borderWidth: "1px",
+      borderColor: "inherit",
+    },
+  }),
+  menu: (provided) => ({
+    // 'menu' is from the div class too.
+    ...provided,
+  }),
+};
+
+export default function SelectComponent({ options }) {
+  return (
+    <Select
+      defaultValue={optionsSample[0]}
+      options={optionsSample}
+      className="w-[500px]"
+      styles={customStyles}
+    />
+  );
+}
