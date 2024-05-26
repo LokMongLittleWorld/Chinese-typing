2e3b3517011e63ef4815face1874ff2738f23815
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-05-26 21:15:08 +0800

    style: fix different height issue

--- react/src/Components/ArticleDisplayCard.jsx
+++ react/src/Components/ArticleDisplayCard.jsx
@@ -1,6 +1,6 @@
 export default function ArticleDisplayCard({ title, content, category }) {
   return (
-    <div className="relative block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div className="relative block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 h-full">
       <div className="flex flex-row items-center justify-between mb-2">
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
