ef6ebe1f34eb2f43f446aa4bc2a4b30bb5ff4013
Leo Lee <1155158461@link.cuhk.edu.hk>
2024-06-09 15:51:12 +0800

    fix: nav bar color not working in some occasion
    refactor the code

--- react/src/Layouts/DefaultLayout.jsx
+++ react/src/Layouts/DefaultLayout.jsx
@@ -1,23 +1,21 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { useStateContext } from "../Contexts/ContextProvider.jsx";
 import axiosClient from "../axios-client.js";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { isStringValid } from "../common/function.js";
 import LoginButton from "../Components/LoginButton.jsx";
 import UserAccountDropdownMenu from "../Components/UserAccountDropdownMenu.jsx";
 
-//"中文速打", "倉頡練習", "粵拼練習"
+const pages = {
+  "speed-typing": "中文速打",
+  "cangjie": "倉頡練習",
+  "jyutping": "粵拼練習",
+};
 export default function DefaultLayout() {
   const { user, token, setUser, setShowAuthenticationModel } =
     useStateContext();
-  const pages = {
-    "speed-typing": "中文速打",
-    "cangjie": "倉頡練習",
-    "jyutping": "粵拼練習",
-  };
-  const [currentPage, setCurrentPage] = useState(
-    sessionStorage.getItem("currentPage") || "speed-typing"
-  );
+
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (isStringValid(token)) {
@@ -32,40 +30,38 @@ export default function DefaultLayout() {
   };
 
   return (
-    <div id="defaultLayout">
-      <div className="w-full p-6">
-        <header className="flex flex-col justify-center items-center mt-2">
-          <div className="relative flex flex-row">
-            <div className="flex flex-row gap-10 text-4xl font-bold">
-              {Object.entries(pages).map(([key, value]) => {
-                return (
-                  <Link
-                    onClick={() => {
-                      setCurrentPage(key);
-                      sessionStorage.setItem("currentPage", key);
-                    }}
-                    key={key}
-                    to={"/" + key}
-                    className={`hover:text-blue-300 hover:-translate-y-0.5 transition-all duration-500 select-none ${
-                      currentPage === key ? "text-blue-500" : "text-black"
-                    }`}
-                  >
-                    {value}
-                  </Link>
-                );
-              })}
-            </div>
-            <div className="fixed right-8 z-40">
-              {isStringValid(token) ? (
-                <UserAccountDropdownMenu />
-              ) : (
-                <LoginButton handleOnClick={handleLoginOnClick} />
-              )}
-            </div>
+    <div id="defaultLayout" className="w-full p-6">
+      <header className="flex flex-col justify-center items-center mt-2">
+        <div className="relative flex flex-row">
+          {/*nav bar*/}
+          <div className="flex flex-row gap-10 text-4xl font-bold">
+            {Object.entries(pages).map(([key, value]) => {
+              return (
+                <Link
+                  key={key}
+                  to={"/" + key}
+                  className={`hover:text-blue-300 hover:-translate-y-0.5 transition-all duration-500 select-none ${
+                    pathname.startsWith("/" + key)
+                      ? "text-blue-500"
+                      : "text-black"
+                  }`}
+                >
+                  {value}
+                </Link>
+              );
+            })}
           </div>
-        </header>
-        <Outlet />
-      </div>
+          {/*user*/}
+          <div className="fixed right-8 z-40">
+            {isStringValid(token) ? (
+              <UserAccountDropdownMenu />
+            ) : (
+              <LoginButton handleOnClick={handleLoginOnClick} />
+            )}
+          </div>
+        </div>
+      </header>
+      <Outlet />
     </div>
   );
 }
